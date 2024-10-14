4937bec9ea0f7616db3eb8edf9fe250d8a57bda6
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-10-14 01:19:25 +0100

    ci: fix nix build

--- default.nix
+++ default.nix
@@ -1,7 +1,10 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ lib, pkgs ? import <nixpkgs> { } }:
 let manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
 pkgs.rustPlatform.buildRustPackage {
+  nativeBuildInputs = [ pkgs.pkg-config ];
+  OPENSSL_NO_VENDOR = 1;
+  buildInputs = [ pkgs.openssl ];
   pname = manifest.name;
   version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
